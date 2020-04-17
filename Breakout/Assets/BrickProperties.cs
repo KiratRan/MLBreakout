db5b36ffa1b9ab3962193d577c61cdc2496409c5
loganseals <loganseals@users.noreply.github.com>
2020-04-16 22:44:56 -0500

    Display & Code Update
    Lives and score on the GUI are now updated properly. Updated and modified code. Added code comments.

--- Breakout/Assets/BrickProperties.cs
+++ Breakout/Assets/BrickProperties.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// using these to parse strings and access the TextMeshProUGUI component of an object
+using System;
+using TMPro;
+
 public class BrickProperties : MonoBehaviour
 {
+
+	// set public variables that can be set in Unity; different blocks will have different
+	// point values and the bricks on either side of two player mode will have a different ball
+	// and different scor objects
     public GameObject myBall;
-    public GameObject myDisplay;
+    public GameObject scoreObject;
+    public int points;
+
+    // this is the variable that will hold the TextMeshProUGUI and allows us
+    // to access and change the text displayed
+    private TextMeshProUGUI ugui;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+    	// get the component of the current score object for the ugui
+    	ugui = scoreObject.GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
@@ -19,14 +33,37 @@ public class BrickProperties : MonoBehaviour
         
     }
 
+    // after something collides with the brick
     void OnCollisionExit2D(Collision2D col){
 
+    	// if the object that collided has the same name as our ball, then disable
+    	// the collider for the brick and stop displaying it on screen
     	if(col.collider.name == myBall.name){
-    		// Debug.Log("Ball - Brick Collision");
     		
+    		// disable components
     		gameObject.GetComponent<SpriteRenderer>().enabled = false;
     		gameObject.GetComponent<BoxCollider2D>().enabled = false;
+
+    		// call function to update the score on the screen appropriately
+    		IncreaseTMProUGUIText(ugui, points);
     		
     	}
+    }
+
+    // This is a function to update the integer value of the text in a TextMeshProUGUI. The
+    // function takes in a TextMeshProUGUI component, and the integer value to increase the
+    // value of the text in the TextMeshProUGUI. The TextMeshProUGUI must already have an integer value
+    // in the text field. THe function does not return anything.
+    void IncreaseTMProUGUIText(TextMeshProUGUI textUGUI, int change){
+
+    	// get the current text value as a string from the textmeshprougui
+    	string curVal = textUGUI.text;
+
+    	// calculate the new score by converting the string from the text to a long and adding the 
+    	// points variable to it
+    	long newVal = Int64.Parse(curVal) + (long)change;
+
+    	// update the text for the textmeshprougui with the new score
+    	textUGUI.text = newVal.ToString();
     }
 }
--- Breakout/Assets/PaddleMovement.cs
+++ Breakout/Assets/PaddleMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PaddleMovement : MonoBehaviour
 {
+    // public variable that can be changed by the user to set the sensitivity of the paddle
+    // should be able to change in settings/controls
 	public float sens = 5.0f;
 
     // Start is called before the first frame update
@@ -18,8 +20,12 @@ public class PaddleMovement : MonoBehaviour
         
     }
 
+    // used to update the physics of the paddle
     void FixedUpdate(){
 
+        // update the x position of the paddle using the transform.Translate property
+        // get the horizontal axis input, multiply by delta time so that the speed change is fixed, and multiply by the sensitivity
+        // no change in y or z axes
     	gameObject.transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * sens,0,0);
     }
 }
